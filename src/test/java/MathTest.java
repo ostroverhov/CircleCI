4dc42ab0c3e7b18782d72061ee1d8664a2f1d9d7
U.Astraverkhau <U.Astraverkhau@itechart-group.com>
2021-08-31 09:45:30 +0300

    add new test

---
+++ src/test/java/MathTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MathTest {
+
+    @Test
+    public void testSum() {
+        Assertions.assertEquals(2 + 2, 4, "check sum");
+    }
+}
